fe1f08e18a1c2228cf99e80bcf1e5942bedad4ba
Oleg Makhnichev <oleg.mahnichev@gmail.com>
2024-05-07 15:45:37 +0200

    modals_form

--- TEMP/tailwind.config.js
+++ TEMP/tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#F3743D'
+        primary: '#F3743D',
+        gray: '#939393',
+        black: '#2C2C2C'
       }
     }
   },
